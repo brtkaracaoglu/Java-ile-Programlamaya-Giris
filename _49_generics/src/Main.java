0eaba7b57ad326e2defe17e3cb56b71250f8641b
Berat Karacaoglu <brtkaracaoglu@gmail.com>
2023-02-21 13:42:07 +0300

    Java Kursu

---
+++ _49_generics/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> sehirler = new ArrayList<String>();
+        sehirler.add("Ankara");
+        sehirler.add("İzmir");
+        sehirler.add("İstanbul");
+
+        MyList<Customer> sehirler1 = new MyList<Customer>();
+        sehirler1.add(new Customer());
+        sehirler1.add(new Customer());
+
+
+    }
+}
